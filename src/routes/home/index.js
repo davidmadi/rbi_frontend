04b0d5277be7d2326372bbc9c0254d18abc031a1
David Madi <davidmadi@gmail.com>
2020-09-20 15:02:14 -0400

    Dettach home from render

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,6 +3,36 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { loadAll } from '../../actions/actions';
 
+const UnloadedHome = ()=> {
+  return (
+    <div className="d-flex flex-column justify-content-md-center h-100 home">
+    <div className="p-2 h-50"></div>
+    <div className="p-2">
+      <div className="d-flex justify-content-center">
+        <span type="button" className="btn btn-primary">LOADING...</span>
+      </div>
+    </div>
+  </div>
+  )
+};
+
+
+const LoadedHome = ()=> {
+  return (
+    <div className="d-flex flex-column justify-content-md-center h-100 home">
+    <div className="p-2 h-50"></div>
+    <div className="p-2">
+      <div className="d-flex justify-content-center">
+        <Link to="/menu/root">
+          <span type="button" className="btn btn-primary">LET'S EAT</span>
+        </Link>
+      </div>
+    </div>
+  </div>
+)
+};
+
+
 class Home extends Component {
 
   componentDidMount(){
@@ -10,40 +40,11 @@ class Home extends Component {
       loadAll(this.props.dispatch);
   }
 
-  unloadedHome = ()=>{
-    return (
-      <div className="d-flex flex-column justify-content-md-center h-100 home">
-        <div className="p-2 h-50"></div>
-        <div className="p-2">
-          <div className="d-flex justify-content-center">
-            <span type="button" className="btn btn-primary">LOADING...</span>
-          </div>
-        </div>
-      </div>
-    );
-    
-  }
-
-  loadedHome = ()=>{
-    return (
-      <div className="d-flex flex-column justify-content-md-center h-100 home">
-        <div className="p-2 h-50"></div>
-        <div className="p-2">
-          <div className="d-flex justify-content-center">
-            <Link to="/menu/root">
-              <span type="button" className="btn btn-primary">LET'S EAT</span>
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   render(){
     if(this.props.mainLoaded)
-      return this.loadedHome();
+      return <LoadedHome/>;
     else
-      return this.unloadedHome();
+      return <UnloadedHome/>;
   }
 
 }
